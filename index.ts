e4dcd3db78725a28c168dfb8f1c8c96b8a740a7f
Your Namecls <junejowaseem0086@gmail.com>
2024-06-07 22:52:43 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env nodecls
+
+import inquirer from "inquirer";
+
+const currency : any = {
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.78,
+    INR: 83.46,
+    PKR: 285,
+    AUD: 1.50,
+    POUND: 0.78
+
+};
+
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: 'from',
+            message: 'Enter From Currency',
+            type: 'list',
+            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'AUD', 'POUND']
+        },
+        {
+            name: 'to',
+            message: 'Enter To Currency',
+            type: 'list',
+            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'AUD', 'POUND']
+        },
+        {
+            name: 'amount',
+            message: 'Enter Your Amount',
+            type: 'number',
+            
+        },
+    ]
+)
+
+
+let fromAmount = currency[user_answer.from]
+let toamount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount  
+let convertedAmount = baseAmount * toamount
+console.log(convertedAmount);
+
